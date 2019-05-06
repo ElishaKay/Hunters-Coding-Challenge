9824662c8a1a7cc082898dae7f7e836267217771
elishakay <kramer1346@gmail.com>
2019-05-06 14:16:59 +0300

    more notes

--- rules.js
+++ rules.js
@@ -14,25 +14,55 @@ H is the amount of already seated treasure hunters.
 -Continues with B lines containing two integers -“XY”, marking theposition of the box on the magic carpet.
 -Ends with Hlines containing two integers -“XY”, marking the position of the hunteron the magic carpet.
 
+--------------------------
+
+First set (task-1.txt)
+- Includes 200 test cases.
+- The length and width of the magic carpet (N) is guaranteed to be an even number between 2 and 100000.
 
 
+-The number of boxes (B) is guaranteed to be a number between 0 and 10000.
+
+-The number of hunters (H) is guaranteed to be a number between 0 and 10000.
+
+-------------------------------
 
 Strategy Ideas:
+
+How do we loop through all different squareMeters to determine whether there is a Hunter / Box there?
+
+How do we determine how filling an emptyPosition will affect the:
+---> leftSideWeight, rightSideWeight, frontSideWeight, backSideWeight 
+
+How do we create an algorithm
+
+-----------------
+
+More Strategy Ideas:
 
 For each test case, take the width and height of the square (N*N), to begin doing calculations.
 
 let squareMeters = ______ ;
 
-let leftSide = 0;
-let rightSide = 0;
-let frontSide = 0;
-let backSide = 0;
+let leftSideTiles = 0;
+let rightSideTiles = 0;
+let frontSideTiles = 0;
+let backSideTiles = 0;
+
+leftSideWeight, 
+rightSideWeight, 
+frontSideWeight, 
+backSideWeight
+
+let numOfAvailablePositions = 0;
 
 
 
 Question:
 
 How do we balance the carpet both left to right and front to back?
+
+
 
 Note:
 
@@ -43,11 +73,19 @@ Boxes are weightless.
 Example #1: How do we simulate the logic with code?
 
 a) There are 16 boxes (4*4)
+b) 
+leftSideWeight = 2
+rightSideWeight = 2
+frontSideWeight = 0
+backSideWeight = 2
 
+Once we calculate the weight of each quadrant, how do we know whether we can add weight to one of the quarants?
+----> And then how much weight we need to add.
 
+Implementation:
 
+Maybe an array of objects:
 
-
-
+[{x: 1, y: 1, empty: true/false, weight: 0/1}, {...}, ...]
 
 
