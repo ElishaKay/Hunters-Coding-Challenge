868df6f7d122900e5bf59ae7715f50405468bb26
elishakay <kramer1346@gmail.com>
2019-05-06 13:38:34 +0300

    more notes

--- rules.js
+++ rules.js
@@ -1,16 +1,18 @@
 Input:
 
-Your input is guaranteedto be valid and you don’t need to implement input validation.
+Your input is guaranteed to be valid and you don’t need to implement input validation.
 
 The first line of the input gives the number of test cases.
 
-Each test case: -Starts with 1line containing three integers -“NBH”, where 
+Each test case: -Starts with 1 line containing three integers -“NBH”, where 
 
 N is the length and width of the magic carpet, 
 
 B is the amount of already placed boxes, 
 
-H is the amount of already seated treasure hunters.-Continues with Blines containing two integers -“XY”, marking theposition of the box on the magic carpet.-Ends with Hlines containing two integers -“XY”, marking the position of the hunteron the magic carpet.
+H is the amount of already seated treasure hunters.
+-Continues with B lines containing two integers -“XY”, marking theposition of the box on the magic carpet.
+-Ends with Hlines containing two integers -“XY”, marking the position of the hunteron the magic carpet.
 
 
 
@@ -20,6 +22,32 @@ Strategy Ideas:
 For each test case, take the width and height of the square (N*N), to begin doing calculations.
 
 let squareMeters = ______ ;
+
+let leftSide = 0;
+let rightSide = 0;
+let frontSide = 0;
+let backSide = 0;
+
+
+
+Question:
+
+How do we balance the carpet both left to right and front to back?
+
+Note:
+
+Boxes are weightless.
+
+--------------------
+
+Example #1: How do we simulate the logic with code?
+
+a) There are 16 boxes (4*4)
+
+
+
+
+
 
 
 
