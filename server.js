3488c1d71d182091be43b009fd6134321c64e3a4
elishakay <kramer1346@gmail.com>
2019-05-08 10:59:35 +0300

    correctly parsing file input within server.js file

--- server.js
+++ server.js
@@ -20,21 +20,25 @@ parseInput=(string)=>{
 	let arr = string.split("\n");
 	let numOfTestCases = arr.slice(0,1);
 	data = arr.slice(1);
-
+	let configs = [];
 	// let configs = data.split("\n");
 
 	// inspect('data:', data)
 	inspect('numOfTestCases:', numOfTestCases)
 	
-	for (let i = 0; i < numOfTestCases;) { 	
+	for (let i = 0; configs.length < numOfTestCases;) {
+		data[i] = data[i].split(" ");
 		N = data[i][0];
-		B = data[i][2];
-		H = data[i][4];
+		B = data[i][1];
+		H = data[i][2];
 		console.log(N, B, H)
+		configs.push(`${N} ${B} ${H}`)
 		i+=(Number(B)+Number(H)+1);
 		console.log('i=', i)	
 	}
 
+	console.log('configs',configs)
+	console.log('configs.length: ',configs.length)
 	// inspect('configs:', configs)
 
 }
