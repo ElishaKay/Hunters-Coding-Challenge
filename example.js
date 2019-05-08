b5f4163c67295600c11bfdcf313c2c907cd226ed
elishakay <kramer1346@gmail.com>
2019-05-08 15:06:04 +0300

    passing at least first 5 tests - much better removeLoad Function - based on mutliple heavy sides

--- example.js
+++ example.js
@@ -33,7 +33,7 @@ parseInput=(string)=>{
 		i=end;
 	}
 
-	for (let i = 0; i< configs.length; i++) {
+	for (let i = 0; i< 20; i++) {
 		calculateMaxHunters(i+1, configs[i]);
 	}
 }
@@ -51,19 +51,13 @@ calculateMaxHunters=(testCaseNumber, config)=>{
 	console.log('grid after fillAllEmptySpaces func: ', grid);
 
 	let weightPerSide = calculateWeightPerSide(grid, N);
-	// console.log('weightPerSide: ',weightPerSide)
+	console.log('weightPerSide: ',weightPerSide)
 
 	let balanceReport = generateBalanceReport(weightPerSide);
-	// console.log('balanceReport: ',balanceReport)
+	console.log('balanceReport: ',balanceReport)
 
 	grid = removeLoad(grid, balanceReport, N);
 	
-	weightPerSide = calculateWeightPerSide(grid, N);
-	balanceReport = generateBalanceReport(weightPerSide);
-
-	if(balanceReport){
-		removeLoad(grid, balanceReport, N);
-	}
 
 	logResults(grid, testCaseNumber);
 }
@@ -173,33 +167,60 @@ const generateBalanceReport = (weightPerSide) => {
 
 
 const removeLoad = (grid, balanceReport, N) => {
+	if(balanceReport.length>0){
+		let filterFunc;
+
+		let heavySides = balanceReport.map(a => a.heavierSide);
+		console.log('heavySides', heavySides);
+		console.log('balanceReport in removeLoad func',balanceReport)
 	
-	if(balanceReport[0]){	
+		/*top-left quadrant*/
+		if(heavySides.includes('left side') && heavySides.includes('front side')) {
+			filterFunc = ({x,y}, N)=> x <= N/2 && y <= N/2  	
+		} /*bottom-left quadrant*/ else if(heavySides.includes('left side') && heavySides.includes('back side')){
+			filterFunc = ({x,y}, N)=> x <= N/2 && y > N/2
+		} /*top-right quadrant*/ else if(heavySides.includes('right side') && heavySides.includes('front side') ){
+			filterFunc = ({x,y}, N)=> x > N/2 && y <= N/2
+		} /*bottom-right quadrant*/ else if(heavySides.includes('right side') && heavySides.includes('back side') ){
+			filterFunc = ({x,y}, N)=>  x > N/2 && y > N/2
+		} else if(heavySides.includes('left side')){
+			filterFunc = ({x,y}, N)=> x <= N/2
+		} else if(heavySides.includes('right side')){
+			filterFunc = ({x,y}, N)=> x > N/2
+		} else if(heavySides.includes('front side')){
+			filterFunc = ({x,y}, N)=> y <= N/2
+		} else if(heavySides.includes('back side')){
+			filterFunc = ({x,y}, N)=> y > N/2
+		}
+
 		for (let i = 0; i < balanceReport[0].byHowMany;) {
-			const seatIndex = grid.findIndex((seat) => !seat.glued && updateRelevantSeats(seat, balanceReport[0].heavierSide, N) && seat.weight > 0);
-			// console.log('ran hunter removal process on: ', grid[seatIndex]);
+			const seatIndex = grid.findIndex((seat) => !seat.glued && filterFunc(seat, N) && seat.weight > 0);
+				// console.log('ran hunter removal process on: ', grid[seatIndex]);
+				
 				if(seatIndex){
 					grid[seatIndex] = {...grid[seatIndex], empty: true, weight: 0}
 					i++;
 				}
 		}
 	}
-	// balanceReport = balanceReport.slice(1)
 
-	// return {grid, balanceReport};
 	return grid;
 }
 
-updateRelevantSeats = ({x,y}, section, N) => {
-	if(section==='left side'){
-		return x <= N/2
-	} else if(section==='right side'){
-		return x > N/2
-	} else if(section==='front side'){
-		return y <= N/2
-	} else if(section==='back side'){
-		return y > N/2
-	}
+updateRelevantSeats = (seat, section, N) => {
+	
+	console.log(`ran updateRelevantSeats func with params seat: ${seat}  section: ${section}  N: ${N}`)	 
+
+
+	// if(section==='left side'){
+	// 	return seat.x <= N/2
+	// } else if(section==='right side'){
+	// 	return seat.x > N/2
+	// } else if(section==='front side'){
+	// 	return seat.y <= N/2
+	// } else if(section==='back side'){
+	// 	return seat.y > N/2
+	// }
 }
 
 
